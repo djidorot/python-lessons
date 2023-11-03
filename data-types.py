b727f308fb4a78a0f641655a3bcdf7998d96c0ef
djhi12 <ido20002@byui.edu>
2023-11-03 20:39:00 +0800

    Data Types

---
+++ data-types.py
@@ -0,0 +1,95 @@
+
+# Integers (int):
+x = 10
+y = -5
+
+
+# Floating-Point Numbers (float):
+pi = 3.14
+e = 2.71828
+
+
+# Strings (str):
+name = "Alice"
+message = 'Hello, World!'
+
+
+# Lists (list):
+fruits = ["apple", "banana", "cherry"]
+numbers = [1, 2, 3, 4, 5]
+
+
+# Tuples (tuple):
+point = (3, 4)
+colors = ("red", "green", "blue")
+
+
+# Dictionaries (dict):
+person = {"name": "Bob", "age": 30}
+student = {"id": 12345, "name": "Alice", "grade": "A"}
+
+
+# Boolean (bool):
+is_active = True
+is_empty = False
+
+
+# NoneType (None):
+result = None
+
+
+# Sets (set):
+fruits = {"apple", "banana", "cherry"}
+colors = set(["red", "green", "blue"])
+
+
+# Example Program
+# Integer example
+x = 5
+y = 3
+sum_of_integers = x + y
+print("Sum of integers:", sum_of_integers)
+
+# Float example
+pi = 3.14
+radius = 2.0
+area_of_circle = pi * radius ** 2
+print("Area of circle:", area_of_circle)
+
+# String example
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print("Full name:", full_name)
+
+# List example
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+print("Fruits:", fruits)
+
+# Tuple example
+point = (3, 4)
+x_coord, y_coord = point
+print("Coordinates:", x_coord, y_coord)
+
+# Dictionary example
+student = {"id": 12345, "name": "Alice", "grade": "A"}
+print("Student ID:", student["id"])
+
+# Boolean example
+is_raining = False
+is_sunny = True
+if is_raining:
+    print("Take an umbrella.")
+elif is_sunny:
+    print("Enjoy the sunshine!")
+
+# NoneType example
+result = None
+if result is None:
+    print("No result available.")
+
+# Set example
+colors = {"red", "green", "blue"}
+colors.add("yellow")
+print("Colors:", colors)
