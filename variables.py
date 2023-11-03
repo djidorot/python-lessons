d95854cdae30eaf2007ef6ea702bb5ad2b3616d3
djhi12 <ido20002@byui.edu>
2023-11-03 21:23:32 +0800

    Variables

---
+++ variables.py
@@ -0,0 +1,32 @@
+
+# Variable Assignment:
+my_variable = 42
+
+
+# Reassigning Variables:
+my_variable = 42
+my_variable = "Hello"
+
+
+# Printing Variables:
+my_variable = 42
+print(my_variable)
+
+
+# Deleting Variables:
+my_variable = 42
+del my_variable
+
+
+# Example Program
+# Input: Length and width of the rectangle
+length = 5
+width = 3
+
+# Calculate the area of the rectangle
+area = length * width
+
+# Display the results
+print("Rectangle Length:", length)
+print("Rectangle Width:", width)
+print("Rectangle Area:", area)
