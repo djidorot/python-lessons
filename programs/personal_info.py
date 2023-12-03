db857eb1ad98289a5e2feba08016a15a3f41837b
djhi12 <ido20002@byui.edu>
2023-12-04 01:29:03 +0800

    Initial commit

---
+++ programs/personal_info.py
@@ -0,0 +1,35 @@
+"""
+Save the File:
+Save the file with an appropriate name and a .py extension, for example, personal_info_program.py.
+
+Run the Program:
+Run the program by executing the file in your Python environment.
+
+Follow Prompts:
+The program will prompt you to enter your name, age, city, and occupation. Follow the prompts and enter the requested information.
+
+Review Output:
+After entering the information, the program will display the collected details. Review the output to ensure it shows the correct information.
+
+Customize the Program (Optional):
+Optionally, you can customize the program:
+Add more input prompts for additional information.
+Modify the output format.
+
+Experiment with additional features based on what you've learned.
+"""
+
+# Personal Information Program
+
+# Get user input for personal information
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+occupation = input("Enter your occupation: ")
+
+# Display the collected information
+print("\nPersonal Information:")
+print("Name: ", name)
+print("Age: ", age)
+print("City: ", city)
+print("Occupation: ", occupation)
