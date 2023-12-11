d84bd94a3a0311012221d568bdfd0f82061f0f72
djhi12 <ido20002@byui.edu>
2023-12-12 07:52:25 +0800

    Initial commit

--- programs/bmi.py
+++ programs/bmi.py
@@ -1,15 +1,14 @@
-
-#
 h = float(input("\nEnter your height in meters: "))
 w = float(input("Enter your weight in kilograms: "))
 
 BMI = w / (h ** 2)
+BMI = round(BMI, 2)  # Round BMI to two decimal places
 
 if BMI < 18.5:
-    print("Your BMI is", BMI, "which means you are underweight. \n")
-elif BMI >= 18.5 and BMI < 25:
-    print("Your BMI is", BMI, "which means you are normal.\n")
-elif BMI >= 25 and BMI < 30:
-    print("Your BMI is", BMI, "which means you are overweight.\n")
+    print(f"Your BMI is {BMI}, which means you are underweight.\n")
+elif 18.5 <= BMI < 25:
+    print(f"Your BMI is {BMI}, which means you are normal.\n")
+elif 25 <= BMI < 30:
+    print(f"Your BMI is {BMI}, which means you are overweight.\n")
 else:
-    print("Your BMI is", BMI, "which means you are obese.\n")
+    print(f"Your BMI is {BMI}, which means you are obese.\n")
