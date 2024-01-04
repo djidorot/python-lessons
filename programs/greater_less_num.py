4fe893e0bc86e3d19aa7b9aa5dea232a682dcc11
djhi12 <ido20002@byui.edu>
2024-01-05 06:44:08 +0800

    Initial commit

--- programs/greater_less_num.py
+++ programs/greater_less_num.py
@@ -6,7 +6,7 @@ example of how to take user input, use conditional statements, and display resul
 """
 
 # Get input from the user
-num1 = float(input("Enter the first number: "))
+num1 = float(input("/nEnter the first number: "))
 num2 = float(input("Enter the second number: "))
 
 # Compare the two numbers
