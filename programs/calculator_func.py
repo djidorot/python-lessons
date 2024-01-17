e464e7e1ec91f7032f0f328c4ec44a7fced49f83
djhi12 <ido20002@byui.edu>
2024-01-18 07:28:07 +0800

    Initial commit

---
+++ programs/calculator_func.py
@@ -0,0 +1,20 @@
+def perform_arithmetic_operations():
+    # Get user input for two numbers
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    # Perform arithmetic operations
+    sum_result = num1 + num2
+    difference_result = num1 - num2
+    product_result = num1 * num2
+    division_result = num1 / num2
+
+    # Display the results
+    print(f"Sum: {num1} + {num2} = {sum_result}")
+    print(f"Difference: {num1} - {num2} = {difference_result}")
+    print(f"Product: {num1} * {num2} = {product_result}")
+    print(f"Division: {num1} / {num2} = {division_result}")
+
+
+# Call the function to execute the code
+perform_arithmetic_operations()
---
+++ programs/sample.py
@@ -0,0 +1,16 @@
+def main(name):
+    return name
+
+
+myname = main("Daniel")
+
+print(myname)
+
+print()
+
+
+def another_main():
+    print("Another Me")
+
+
+another_main()
