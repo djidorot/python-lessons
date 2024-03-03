78622ff449b01034b0dee5a34c2ce3b293057b14
djhi12 <ido20002@byui.edu>
2024-03-04 01:34:02 +0800

    updated

--- updated-lesson/triple_quoted_strings.py
+++ updated-lesson/triple_quoted_strings.py
@@ -1,3 +1,6 @@
+"""
+Create 9 sentences in python using single quotes, double quotes, and multiline. 3 each with escape character
+"""
 
 sentence1 = 'She said, \'Hello, how are you?\''
 sentence2 = "He exclaimed, \"That's amazing!\""
@@ -26,3 +29,24 @@ print(sentence6)
 print(sentence7)
 print(sentence8)
 print(sentence9)
+
+
+##########################################################################
+
+print('She said, \'Hello, how are you?\'')
+print("He exclaimed, \"That's amazing!\"")
+print('''This is a multiline string.
+It spans across multiple lines.
+Don\'t forget to escape special characters.''')
+
+print('They whispered, \'We\'ll meet again.\'')
+print("The sign read, \"Don't touch.\"")
+print("""This is a multiline string.
+It uses "double quotes" for emphasis.
+Remember to escape special characters like \"this\".""")
+
+print('The cat meowed, \'Feed me!\'.')
+print("The dog barked, \"Let's play fetch!\"")
+print('''This is a multiline string.
+It contains 'single quotes' and escape characters.
+Don\'t overlook their importance.''')
