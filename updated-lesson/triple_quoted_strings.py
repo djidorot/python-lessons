effc199a67ad12f9a468fb259b7749f8e2ad0eff
djhi12 <ido20002@byui.edu>
2024-03-04 01:32:15 +0800

    updated

---
+++ updated-lesson/triple_quoted_strings.py
@@ -0,0 +1,28 @@
+
+sentence1 = 'She said, \'Hello, how are you?\''
+sentence2 = "He exclaimed, \"That's amazing!\""
+sentence3 = '''This is a multiline string.
+It spans across multiple lines.
+Don\'t forget to escape special characters.'''
+
+sentence4 = 'They whispered, \'We\'ll meet again.\''
+sentence5 = "The sign read, \"Don't touch.\""
+sentence6 = """This is a multiline string.
+It uses "double quotes" for emphasis.
+Remember to escape special characters like \"this\"."""
+
+sentence7 = 'The cat meowed, \'Feed me!\'.'
+sentence8 = "The dog barked, \"Let's play fetch!\""
+sentence9 = '''This is a multiline string.
+It contains 'single quotes' and escape characters.
+Don\'t overlook their importance.'''
+
+print(sentence1)
+print(sentence2)
+print(sentence3)
+print(sentence4)
+print(sentence5)
+print(sentence6)
+print(sentence7)
+print(sentence8)
+print(sentence9)
