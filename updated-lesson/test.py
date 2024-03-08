0faabff1ae85fd478feab0710433d853622a67f3
djhi12 <ido20002@byui.edu>
2024-03-09 03:04:16 +0800

    updated

---
+++ updated-lesson/test.py
@@ -0,0 +1,6 @@
+45 + 72
+x = 10
+
+print(45+72)
+print(x)
+print('Daniel')
---
+++ updated-lesson/variables_and_assignment.py
@@ -0,0 +1,32 @@
+"""
+Problem Statement:
+
+Suppose you're tasked with solving a series of mathematical operations and analyzing some Python code. Here are the tasks you need to complete:
+
+Calculate the sum of 45 and 72.
+Assign the value 7 to the variable x and the value 3 to the variable y.
+Find the sum of variables x and y.
+Store the sum of variables x and y in a variable named "total".
+Determine the data type of variable x.
+Determine the data type of the number 10.5.
+Lastly, print the results of each calculation and display the data types of variables x and 10.5.
+
+"""
+
+45 + 72
+x = 7
+y = 3
+x + y
+total = x + y
+type(x)
+type(10.5)
+
+print()
+print(45 + 72)
+print(x)
+print(y)
+print(x + y)
+print(total)
+print(type(x))
+print(type(10.5))
+print()
