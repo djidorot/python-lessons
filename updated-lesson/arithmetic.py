ace9fc30e1da7d7e16aa0c92c757a37440794c78
djhi12 <ido20002@byui.edu>
2024-03-15 01:29:15 +0800

    updated

---
+++ updated-lesson/arithmetic.py
@@ -0,0 +1,35 @@
+"""
+Suppose we have two variables, a and b, with the following values:
+
+a = 4
+b = 5
+We also have the following expressions:
+
+result1 = 3 * (a - b)
+result2 = 3 * a - b
+result3 = 4 ** 3 ** 2
+result4 = (4 ** 3) ** 2
+result5 = 4 ** (3 ** 2)
+
+Your task is to evaluate each of these expressions and determine their respective results.
+"""
+
+# Define variables
+a = 4
+b = 5
+
+# Calculate and print the results
+result1 = 3 * (a - b)
+print("Result 1:", result1)
+
+result2 = 3 * a - b
+print("Result 2:", result2)
+
+result3 = 4 ** 3 ** 2
+print("Result 3:", result3)
+
+result4 = (4 ** 3) ** 2
+print("Result 4:", result4)
+
+result5 = 4 ** (3 ** 2)
+print("Result 5:", result5)
---
+++ updated-lesson/object_dynamic.py
@@ -0,0 +1,14 @@
+"""
+Problem Statement: Suppose you have a rectangular garden with a length of 7.5 meters and a width of 3 meters. Calculate the total area of the garden.
+
+Task for Students: Write an expression to find the area of the rectangular garden using the given dimensions.
+
+Remember to use the formula for the area of a rectangle: Area = Length x Width.
+
+What is the area of the garden?
+
+"""
+Area = 7.5 * 3
+
+type(Area)
+print(Area)
