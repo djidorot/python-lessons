2e699f7c76e960e28ccfe0a3cb151d880d4016b4
DJ Idorot <ido20002@byui.edu>
2025-03-13 06:17:54 +0800

    updated

---
+++ list-double/list_of_names_append_clear.py
@@ -0,0 +1,18 @@
+# Create an empty list
+names = []
+
+# Use a while loop to add names to the list
+while True:
+    # Get user input
+    name = input(
+        "\nEnter a name (or type 'done' to finish, 'clear' to reset): ")
+    if name.lower() == 'done':  # Exit condition
+        break
+    elif name.lower() == 'clear':  # Clear the list
+        names.clear()
+        print("List cleared!")
+    else:
+        names.append(name)  # Add the name to the list
+
+# Print the updated list of names
+print(names)
