b681a8a3d36d4411deb9fff55c58077c0c50503a
DJ Idorot <ido20002@byui.edu>
2025-03-20 05:39:26 +0800

    updated

---
+++ list-double/list_names_append_copy.py
@@ -0,0 +1,15 @@
+# Create an empty list
+names = []
+
+# Use a while loop to add names to the list
+while True:
+    name = input("Enter a name (or type 'done' to finish): ")  # Get user input
+    if name.lower() == 'done':  # Exit condition
+        break
+    names.append(name)  # Add the name to the list
+
+# Create a copy of the list
+names_copy = names.copy()
+
+# Print the updated list of names (copied version)
+print(names_copy)
